11129462781bb85299a5466748d935617e42560d
Samantha Ouyang <friendly304010@gmail.com>
2025-07-22 15:54:37 -0700

    100x 7/22

--- src/config.py
+++ src/config.py
@@ -8,6 +8,6 @@ Date: 07/01/2025
 """
 
 # Model training parameters
-N_REPEATS = 2  # Number of training runs for statistical significance
+N_REPEATS = 100  # Number of training runs for statistical significance
 TEST_SIZE = 0.2  # Fraction of data to use for testing (20%)
-PRETERM_CUTOFF = 37  # Gestational age cutoff for preterm classification (weeks)
+PRETERM_CUTOFF = 37  # Gestational age cutoff for preterm classification (weeks)ß
